5ae6f5b6fc4c7d79195df2e516f3b99b6eca248e
Guillaume <guillaume_boue@orange.fr>
2014-03-16 20:49:44 +0100

    Lecture de la sortie console d'ImageMagick pour détecter ses erreurs.

--- compressito/src/main/java/fr/tunaki/compressito/exec/CompressExec.java
+++ compressito/src/main/java/fr/tunaki/compressito/exec/CompressExec.java
@@ -1,6 +1,8 @@
 package fr.tunaki.compressito.exec;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,58 +26,73 @@ import org.slf4j.LoggerFactory;
  */
 public class CompressExec extends AbstractThread {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CompressExec.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(CompressExec.class);
 
-    private String imagesPath;
-    private String imageMagickPath;
+	private String imagesPath;
+	private String imageMagickPath;
 
-    /**
-     * Construct this object with the given path to images and ImageMagick
-     * @param imagesPath Path to images
-     * @param imageMagickPath Path to ImageMagick installation
-     */
-    public CompressExec(String imagesPath, String imageMagickPath) {
-        this.imageMagickPath = imageMagickPath;
-        this.imagesPath = imagesPath;
-    }
+	/**
+	 * Construct this object with the given path to images and ImageMagick
+	 * @param imagesPath Path to images
+	 * @param imageMagickPath Path to ImageMagick installation
+	 */
+	public CompressExec(String imagesPath, String imageMagickPath) {
+		this.imageMagickPath = imageMagickPath;
+		this.imagesPath = imagesPath;
+	}
 
-    /**
-     * Start the compression.
-     * <p>
-     * The compressing operation operates in its own thread.
-     * <p>
-     * A {@link CompressionException} is thrown if an exception occurs during the compression process.
-     */
-    @Override
-    public void doRun() {
-        try {
-            Process p = new ProcessBuilder()//
-                    .command(Paths.get(imageMagickPath, "mogrify.exe").toString(), "-format", "jpg", "-strip", "-quality", "75", "-compress", "JPEG", "*.*")//
-                    .directory(Paths.get(imagesPath).toFile())//
-                    .start();
-            p.waitFor();
-        } catch (IOException | InterruptedException e) {
-            String message = "Exception lors de la compression des images";
-            LOGGER.error(message, e);
-            throw new CompressionException(message, e);
-        }
-        deleteAllNonJpegImages(Paths.get(imagesPath));
-    }
+	/**
+	 * Start the compression.
+	 * <p>
+	 * The compressing operation operates in its own thread.
+	 * <p>
+	 * A {@link CompressionException} is thrown if an exception occurs during the compression process.
+	 */
+	@Override
+	public void doRun() {
+		try {
+			Process p = new ProcessBuilder()
+			.command(Paths.get(imageMagickPath, "mogrify.exe").toString(), "-format", "jpg", "-strip", "-quality", "75", "-compress", "JPEG", "*.*")
+			.directory(Paths.get(imagesPath).toFile())
+			.redirectErrorStream(true)
+			.start();
+			StringBuilder sb = new StringBuilder();
+			int returnCode;
+			try (
+					InputStreamReader isr = new InputStreamReader(p.getInputStream());
+					BufferedReader br = new BufferedReader(isr);) {
+				String line;
+				while ((line = br.readLine()) != null) {
+					LOGGER.info(line);
+					sb.append(line).append(System.lineSeparator());
+				}
+				returnCode = p.waitFor();
+			}
+			if (returnCode != 0) {
+				throw new CompressionException("Une erreur est survenue lors de la compression des images : " + sb);
+			}
+			deleteAllNonJpegImages(Paths.get(imagesPath));
+		} catch (IOException | InterruptedException e) {
+			String message = "Exception lors de la compression des images : " + e.getMessage();
+			LOGGER.error(message, e);
+			throw new CompressionException(message, e);
+		}
+	}
 
-    private void deleteAllNonJpegImages(Path imagesPath) {
-        try (DirectoryStream<Path> ds = Files.newDirectoryStream(imagesPath, new DirectoryStream.Filter<Path>() {
-            @Override
-            public boolean accept(Path entry) throws IOException {
-                return !entry.toString().endsWith(".jpg");
-            }
-        });) {
-            for (Path path : ds) {
-                Files.delete(path);
-            }
-        } catch (IOException e) {
-            String message = "Exception lors de la suppression des fichiers non compressés";
-            LOGGER.error(message, e);
-            throw new CompressionException(message, e);
-        }
-    }
+	private void deleteAllNonJpegImages(Path imagesPath) {
+		try (DirectoryStream<Path> ds = Files.newDirectoryStream(imagesPath, new DirectoryStream.Filter<Path>() {
+			@Override
+			public boolean accept(Path entry) throws IOException {
+				return !entry.toString().endsWith(".jpg");
+			}
+		});) {
+			for (Path path : ds) {
+				Files.delete(path);
+			}
+		} catch (IOException e) {
+			String message = "Exception lors de la suppression des fichiers non compressés";
+			LOGGER.error(message, e);
+			throw new CompressionException(message, e);
+		}
+	}
 }
--- compressito/src/main/java/fr/tunaki/compressito/view/MainFrame.java
+++ compressito/src/main/java/fr/tunaki/compressito/view/MainFrame.java
@@ -23,90 +23,91 @@ import fr.tunaki.compressito.i18n.Msg;
  */
 public class MainFrame extends JFrame {
 
-    private static final long serialVersionUID = 4680572811590070535L;
-    private static final int MINIMUM_SIZE = 500;
+	private static final long serialVersionUID = 4680572811590070535L;
+	private static final int MINIMUM_SIZE = 500;
 
-    private EventListenerList localeChangeListenerList = new EventListenerList();
-    private MainMenuBar menuBar;
-    private ConfigPanel configPanel;
-    private JButton compressButton;
+	private EventListenerList localeChangeListenerList = new EventListenerList();
+	private MainMenuBar menuBar;
+	private ConfigPanel configPanel;
+	private JButton compressButton;
 
-    /**
-     * Construct this frame.
-     */
-    public MainFrame() {
-        getContentPane().setLayout(new BorderLayout());
-        menuBar = new MainMenuBar(this);
-        setJMenuBar(menuBar);
-        configPanel = new ConfigPanel(this);
-        compressButton = new JButton(Msg.get("button.compress"));
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.add(compressButton);
-        add(configPanel, BorderLayout.NORTH);
-        add(buttonPanel, BorderLayout.CENTER);
-        compressButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                final OnGoingCompressionDialog compressionDialog = new OnGoingCompressionDialog(MainFrame.this);
-                CompressExec exec = new CompressExec(configPanel.getImagesPath(), configPanel.getImageMagickPath());
-                exec.addStartRunnable(new Runnable() {
-                    @Override
-                    public void run() {
-                        compressionDialog.open();
-                    }
-                });
-                exec.addStopRunnable(new Runnable() {
-                    @Override
-                    public void run() {
-                        compressionDialog.close();
-                        JOptionPane.showMessageDialog(MainFrame.this, Msg.get("compression.success.message"), Msg.get("compression.success.title"), JOptionPane.INFORMATION_MESSAGE);
-                    }
-                });
-                exec.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
-                    @Override
-                    public void uncaughtException(Thread t, Throwable e) {
-                        compressionDialog.close();
-                        JOptionPane.showMessageDialog(MainFrame.this, e.getCause().getMessage(), e.getMessage(), JOptionPane.ERROR_MESSAGE);
-                    }
-                });
-                exec.start();
-            }
-        });
-        setVisible(true);
-        setTitle("Compressito!");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setExtendedState(MAXIMIZED_BOTH);
-        setMinimumSize(new Dimension(MINIMUM_SIZE, MINIMUM_SIZE));
-    }
+	/**
+	 * Construct this frame.
+	 */
+	public MainFrame() {
+		getContentPane().setLayout(new BorderLayout());
+		menuBar = new MainMenuBar(this);
+		setJMenuBar(menuBar);
+		configPanel = new ConfigPanel(this);
+		compressButton = new JButton(Msg.get("button.compress"));
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(compressButton);
+		add(configPanel, BorderLayout.NORTH);
+		add(buttonPanel, BorderLayout.CENTER);
+		compressButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				final OnGoingCompressionDialog compressionDialog = new OnGoingCompressionDialog(MainFrame.this);
+				CompressExec exec = new CompressExec(configPanel.getImagesPath(), configPanel.getImageMagickPath());
+				exec.addStartRunnable(new Runnable() {
+					@Override
+					public void run() {
+						compressionDialog.open();
+					}
+				});
+				exec.addStopRunnable(new Runnable() {
+					@Override
+					public void run() {
+						compressionDialog.close();
+						JOptionPane.showMessageDialog(MainFrame.this, Msg.get("compression.success.message"), Msg.get("compression.success.title"), JOptionPane.INFORMATION_MESSAGE);
+					}
+				});
+				exec.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+					@Override
+					public void uncaughtException(Thread t, Throwable e) {
+						compressionDialog.close();
+						String message = "<html><p style='width: 500px;'>" + e.getMessage();
+						JOptionPane.showMessageDialog(MainFrame.this, message, "Erreur", JOptionPane.ERROR_MESSAGE);
+					}
+				});
+				exec.start();
+			}
+		});
+		setVisible(true);
+		setTitle("Compressito!");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setExtendedState(MAXIMIZED_BOTH);
+		setMinimumSize(new Dimension(MINIMUM_SIZE, MINIMUM_SIZE));
+	}
 
-    /**
-     * Add a listener to the locale change event.
-     * @param l Listener to add.
-     */
-    public void addLocaleChangeListener(LocaleChangeListener l) {
-        localeChangeListenerList.add(LocaleChangeListener.class, l);
-    }
+	/**
+	 * Add a listener to the locale change event.
+	 * @param l Listener to add.
+	 */
+	public void addLocaleChangeListener(LocaleChangeListener l) {
+		localeChangeListenerList.add(LocaleChangeListener.class, l);
+	}
 
-    /**
-     * Remove the given listener from the list of listeners to the locale change event.
-     * @param l Listener to remove.
-     */
-    public void removeFooListener(LocaleChangeListener l) {
-        localeChangeListenerList.remove(LocaleChangeListener.class, l);
-    }
+	/**
+	 * Remove the given listener from the list of listeners to the locale change event.
+	 * @param l Listener to remove.
+	 */
+	public void removeFooListener(LocaleChangeListener l) {
+		localeChangeListenerList.remove(LocaleChangeListener.class, l);
+	}
 
-    /**
-     * Fires the locale change event with the given new locale.
-     * @param locale New locale.
-     */
-    public void fireLocaleChangeEvent(String locale) {
-        Object[] listeners = localeChangeListenerList.getListenerList();
-        for (int i = listeners.length - 2; i >= 0; i -= 2) {
-            if (listeners[i] == LocaleChangeListener.class) {
-                ((LocaleChangeListener) listeners[i + 1]).actionPerformed(new LocaleChangeEvent(this, locale));
-            }
-        }
-        compressButton.setText(Msg.get("button.compress"));
-    }
+	/**
+	 * Fires the locale change event with the given new locale.
+	 * @param locale New locale.
+	 */
+	public void fireLocaleChangeEvent(String locale) {
+		Object[] listeners = localeChangeListenerList.getListenerList();
+		for (int i = listeners.length - 2; i >= 0; i -= 2) {
+			if (listeners[i] == LocaleChangeListener.class) {
+				((LocaleChangeListener) listeners[i + 1]).actionPerformed(new LocaleChangeEvent(this, locale));
+			}
+		}
+		compressButton.setText(Msg.get("button.compress"));
+	}
 
 }
